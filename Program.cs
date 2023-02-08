5bbd3180da69c79a7c3d52cac0a0434c0a54e32f
Setthawut <setthawut.kitd@mail.kmutt.ac.th>
2023-02-08 15:09:48 +0700

    bool

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿class BooleanExample {
+    static void Main(string[] args) {
+        bool x = !(7>=2) && (4 == 2) || (4 < 2);
+        bool y = (3 > 3) || ((5 > 2) && (4 < 1));
+    
+        
+        Console.WriteLine("x = {0}", x);
+        Console.WriteLine("y = {0}", y);
+    }
+}
