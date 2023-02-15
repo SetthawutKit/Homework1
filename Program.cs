fb152189bca651e0af7a3fb2e617fea8f449723c
Setthawut <setthawut.kitd@mail.kmutt.ac.th>
2023-02-15 15:05:20 +0700

    d

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
 ﻿class Program {
     static void Main(string[] args) {
-        Console.Write("Please input name: ");
-        string name = Console.ReadLine();
-        Console.WriteLine("Hello {0}", name);
+        Console.Write("Please input x: ");
+        int x = int.Parse(Console.ReadLine());
+
+        if (x > 0) {
+            Console.WriteLine("Positive")
+        }
     }
 }
+
