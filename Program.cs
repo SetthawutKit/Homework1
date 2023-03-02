2c78ba63957e214c0e57c866a4e229b518ef49f4
Setthawut <setthawut.kitd@mail.kmutt.ac.th>
2023-03-03 02:05:22 +0700

    done

--- Program.cs
+++ Program.cs
@@ -12,8 +12,33 @@ namespace Homework {
                 Console.WriteLine("Error: Passcode must have 6 digits.");
             }
             else if (agent != "FBI" && agent != "NSA" && agent != "CIA") {
-                
+                Console.WriteLine("Error: Invalid agency.");
             }
+            else if (agent == "FBI") {
+                if (pass[0] >= '4' && pass[0] <= '7' && pass[1] % 2 != 0 && pass[3] % 2 == 0) {
+                    Console.WriteLine("Pass is valid.");
+                }
+                else {
+                    Console.WriteLine("Error: Passcode is not valid for this agency.");
+                }
+            }
+            else if (agent == "CIA") {
+                if (pass[5] % 3 == 0 && pass[4] != '1' && pass[4] != '3' && pass[4] != '5' && pass[2] >= 6 && pass[2] != '8') {
+                    Console.WriteLine("Pass is valid.");
+                }
+                else {
+                    Console.WriteLine("Error: Passcode is not valid for this agency.");
+                }
+            }
+            else if (agent == "NSA") {
+                if ((pass[5] == '1' || pass[5] == '2' || pass[5] == '3' || pass[5] == '5' || pass[5] == '6') && pass[4] % 3 == 0 && pass[4] % 2 != 0 && pass.Contains('7')) {
+                    Console.WriteLine("Pass is valid.");
+                }
+                else {
+                    Console.WriteLine("Error: Passcode is not valid for this agency.");
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
